e86ac36b1023d617b4854fccf8c25e6fbddd5cc8
filimonq <filimonqqq@yandex.ru>
2024-09-09 14:00:50 +0700

    cod_style1

--- Task_1_1_1/src/main/java/ru/nsu/fitkulin/Main.java
+++ Task_1_1_1/src/main/java/ru/nsu/fitkulin/Main.java
@@ -2,22 +2,19 @@ package ru.nsu.fitkulin;
 import java.util.Scanner;
 
 public class Main {
-    static void printArray(int[] arr)
-    {
-        int N = arr.length;
+    static void printArray(int[] arr) {
+        int n = arr.length;
         for (int j : arr) {
             System.out.print(j + " ");
         }
         System.out.println();
     }
     static int[] HeapSort(int[] arr) {
-        int N = arr.length;
-        for (int i = N / 2 - 1; i >= 0; i--)
-        {
-            heapify(arr, N, i);  // выносим макс элемент в верх дерева
+        int n = arr.length;
+        for (int i = n / 2 - 1; i >= 0; i--) {
+            heapify(arr, n, i);  // выносим макс элемент в верх дерева
         }
-        for (int i = N - 1; i >= 0; i--)
-        {
+        for (int i = n - 1; i >= 0; i--) {
             int tmp = arr[0]; // меняем макс и мин элементы
             arr[0] = arr[i];
             arr[i] = tmp;
@@ -25,25 +22,21 @@ public class Main {
         }
         return arr;
     }
-    static void heapify(int[] arr, int N, int k)
-    {
+    static void heapify(int[] arr, int n, int k) {
         int curr = k;                           //        curr         //
         int left = 2 * k + 1;                   //       /    \        //
         int right = 2 * k + 2;                  //    left    right    //
-        if (left < N && arr[left] > arr[curr])
-        {
+        if (left < n && arr[left] > arr[curr]) {
             curr = left;
         }
-        if (right < N && arr[right] > arr[curr])
-        {
+        if (right < n && arr[right] > arr[curr]) {
             curr = right;
         }
-        if (curr != k)
-        {
+        if (curr != k) {
             int tmp = arr[k];
             arr[k] = arr[curr];
             arr[curr] = tmp;
-            heapify(arr, N, curr);
+            heapify(arr, n, curr);
         }
     }
     public static void main(String[] args) {
--- Task_1_1_1/src/test/java/ru/nsu/fitkulin/MainTest.java
+++ Task_1_1_1/src/test/java/ru/nsu/fitkulin/MainTest.java
@@ -3,12 +3,13 @@ package ru.nsu.fitkulin;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
+
 class MainTest {
     @Test
     void SampleTest() {
-        assertArrayEquals(new int[] {1,2,3}, Main.HeapSort(new int[] {1,2,3}));
-        assertArrayEquals(new int[] {1,1,1}, Main.HeapSort(new int[] {1,1,1}));
-        assertArrayEquals(new int[] {}, Main.HeapSort(new int[] {}));
-        assertArrayEquals(new int[] {1,2,3,4,5}, Main.HeapSort(new int[] {5,4,3,2,1}));
+        assertArrayEquals(new int[] {1, 2, 3},  Main.HeapSort(new int[] {1, 2, 3}));
+        assertArrayEquals(new int[] {1, 1, 1},  Main.HeapSort(new int[] {1, 1, 1}));
+        assertArrayEquals(new int[] {},  Main.HeapSort(new int[] {}));
+        assertArrayEquals(new int[] {1, 2, 3, 4, 5},  Main.HeapSort(new int[] {5, 4, 3, 2, 1}));
     }
 }
