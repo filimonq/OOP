54f64f6f65d02a58697e5137741a4451c78ec3e8
Ildar Fitkulin <i.fitkulin@g.nsu.ru>
2024-09-20 13:33:42 +0700

    for revue

--- Task_1_1_2/src/test/java/ru/nsu/fitkulin/GameTest.java
+++ Task_1_1_2/src/test/java/ru/nsu/fitkulin/GameTest.java
@@ -18,7 +18,21 @@ public class GameTest {
      */
     @Test
     public void testDealInitialCards() {
-        ByteArrayInputStream input = new ByteArrayInputStream("0\n".repeat(52).getBytes());
+        ByteArrayInputStream input = new ByteArrayInputStream("0\n".repeat(1).getBytes());
+
+        System.setIn(input);
+
+        ByteArrayOutputStream output = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(output));
+
+        Game.round();
+
+        assertTrue(true);
+    }
+    
+    @Test
+    public void testDealInitialCards2() {
+        ByteArrayInputStream input = new ByteArrayInputStream("1\n".repeat(52).getBytes());
 
         System.setIn(input);
 
