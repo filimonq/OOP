c754456f0cf1aa8b4ad2ad5a4d241d7fb37a4eee
Ildar Fitkulin <i.fitkulin@g.nsu.ru>
2024-09-23 21:35:24 +0700

    test build

--- Task_1_1_3/src/main/java/ru/nsu/fitkulin/Main.java
+++ Task_1_1_3/src/main/java/ru/nsu/fitkulin/Main.java
@@ -1,6 +1,9 @@
 package ru.nsu.fitkulin;
 
 public class Main {
+    public static int sum(int a, int b) {
+        return a + b;
+    }
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
