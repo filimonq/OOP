d2e352e44c6a11c1cc01ff316d0b837848b09521
Ildar Fitkulin <i.fitkulin@g.nsu.ru>
2024-10-10 19:41:56 +0700

    test

---
+++ Task_1_1_3/src/test/java/ru/nsu/fitkulin/SimpleTest.java
@@ -0,0 +1,15 @@
+package ru.nsu.fitkulin;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class SimpleTest {
+    @Test
+    void simplificationTestA() {
+        String expression = "(x*0) +(10-0) -  (x*1) + 12 + (128*0) + (32*1)+ 0/ y";
+        String expected = "(((10-x)+12)+32)";
+        String actual = Parser.parse(expression).simple().toString();
+        assertEquals(expected, actual);
+    }
+}
