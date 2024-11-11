1f99784eccd48ed44c611fafae975064d8128063
Ildar Fitkulin <i.fitkulin@g.nsu.ru>
2024-11-11 11:21:58 +0700

    test

---
+++ Task_1_3_1/src/main/java/ru/nsu/fitkulin/SubstringSearch.java
@@ -0,0 +1,11 @@
+package ru.nsu.fitkulin;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class SubstringSearch {
+
+}
---
+++ Task_1_3_1/src/test/java/ru/nsu/fitkulin/SubstringSearchTest.java
@@ -0,0 +1,13 @@
+package ru.nsu.fitkulin;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class SubstringSearchTest {
+    @Test
+    void test() {
+
+    }
+}
