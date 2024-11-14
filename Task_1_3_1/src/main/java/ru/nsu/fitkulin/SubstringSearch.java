5fbd9d3a0ff493bdc6a915e0e1b16ade21fbf4ab
Ildar Fitkulin <i.fitkulin@g.nsu.ru>
2024-11-15 00:18:31 +0700

    ready??

--- Task_1_3_1/src/main/java/ru/nsu/fitkulin/SubstringSearch.java
+++ Task_1_3_1/src/main/java/ru/nsu/fitkulin/SubstringSearch.java
@@ -1,11 +1,85 @@
 package ru.nsu.fitkulin;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
-import java.util.List;
 
 public class SubstringSearch {
 
+    private final String targetString;
+
+    /**
+     * constructor.
+     */
+    public SubstringSearch(String targetString) {
+        this.targetString = targetString;
+    }
+
+    /**
+     * method for finding a substring using the KMP algorithm.
+     */
+    ArrayList<Integer> find(String filePath, int limit) throws IOException {
+        ArrayList<Integer> list = new ArrayList<>();
+        try (BufferedInputStream stream = new BufferedInputStream(new FileInputStream(filePath), limit)) {
+            byte[] buffer = new byte[limit];
+            int bytesRead;
+
+            while ((bytesRead = stream.read(buffer)) != -1) {
+                String partOfFile = new String(buffer, 0, bytesRead, StandardCharsets.UTF_8);
+                kmpSearch(partOfFile, list);
+            }
+        }
+        return list;
+    }
+
+    /**
+     * Knuth–Morris–Pratt algorithm.
+     */
+    private void kmpSearch(String text, ArrayList<Integer> list) {
+        int[] lps = computeLPSArray(targetString);
+        int i = 0; // индекс для текста
+        int j = 0; // индекс для паттерна
+
+        while (i < text.length()) {
+            if (targetString.charAt(j) == text.charAt(i)) {
+                i++;
+                j++;
+            }
+
+            if (j == targetString.length()) {
+                list.add(i - j); // совпадение
+                j = lps[j - 1];
+            } else if (i < text.length() && targetString.charAt(j) != text.charAt(i)) {
+                if (j != 0) {
+                    j = lps[j - 1];
+                } else {
+                    i++;
+                }
+            }
+        }
+    }
+
+    /**
+     * method for fill lps array.
+     */
+    private int[] computeLPSArray(String pattern) {
+        int[] lps = new int[pattern.length()];
+        int length = 0;
+        lps[0] = 0; // lps[0] всегда 0
+
+        for (int i = 1; i < pattern.length(); i++) {
+            while (length > 0 && pattern.charAt(i) != pattern.charAt(length)) {
+                length = lps[length - 1];
+            }
+            if (pattern.charAt(i) == pattern.charAt(length)) {
+                length++;
+                lps[i] = length;
+            } else {
+                lps[i] = 0;
+            }
+        }
+        return lps;
+    }
 }
--- Task_1_3_1/src/test/java/ru/nsu/fitkulin/SubstringSearchTest.java
+++ Task_1_3_1/src/test/java/ru/nsu/fitkulin/SubstringSearchTest.java
@@ -1,13 +1,40 @@
 package ru.nsu.fitkulin;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class SubstringSearchTest {
     @Test
-    void test() {
-
+    void startTest() throws IOException {
+        SubstringSearch actual = new SubstringSearch("бра");
+        ArrayList<Integer> expected = new ArrayList<>(Arrays.asList(1, 8));
+        assertEquals(expected, actual.find("src/test/resources/file1.txt", 1000));
     }
+
+    @Test
+    void testUTF() throws IOException {
+        SubstringSearch actual = new SubstringSearch("你");
+        ArrayList<Integer> expected = new ArrayList<>(List.of(4));
+        assertEquals(expected, actual.find("src/test/resources/file2.txt", 1000));
+    }
+
+    @Test
+    void testEmptyArr() throws IOException {
+        SubstringSearch actual = new SubstringSearch(" ");
+        ArrayList<Integer> expected = new ArrayList<>();
+        assertEquals(expected, actual.find("src/test/resources/file2.txt", 1000));
+    }
+
+    @Test
+    void testLargeFile() throws IOException {
+        SubstringSearch actual = new SubstringSearch("Война");
+        ArrayList<Integer> expected = new ArrayList<>(Arrays.asList(13, 60, 1257, 1496));
+        assertEquals(expected, actual.find("src/test/resources/file3.txt", 10000));
+    }
+
 }
