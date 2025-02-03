3912b0d025d134d7de16cc6e6108af7b94e5df06
Ildar Fitkulin <i.fitkulin@g.nsu.ru>
2025-02-03 18:31:17 +0700

    test

---
+++ Task_2_1_1/src/test/java/ru/nsu/fitkulin/MainTest.java
@@ -0,0 +1,13 @@
+package ru.nsu.fitkulin;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+    @Test
+    void test() {
+        
+    }
+
+}
